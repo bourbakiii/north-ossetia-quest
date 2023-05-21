dd3eade72d2ce4f17fe93072fd8e6bfa44a156e7
bourbakiii <gabisov03912@gmail.com>
2023-05-21 11:44:57 +0300

    .

--- src/layouts/question-layout.vue
+++ src/layouts/question-layout.vue
@@ -1,19 +1,47 @@
 <template>
   <div class="layout question-layout">
-    LAYOUT LABEL
+    <router-link to="/" class="question-layout__main-link main-link">
+      <icon class="main-link__icon" name="arrow-back"/>
+      <p class="main-link__text">на главную</p>
+    </router-link>
+    <br/>
+    <br/>
     <slot class="page" name="page-render">Кажется, что-то пошло не так...</slot>
   </div>
 </template>
 <script setup>
+import Icon from "@/components/icon.vue";
+
 console.log("Layout rendered");
 </script>
 <style lang="scss" scoped>
-.layout{
-  height:100%;
-  width:100%;
+.question-layout {
+  height: 100%;
+  width: 100%;
   padding: 20px;
 }
-.question-page{
-  background-color: rgba(0,0,0,.1);
+
+.question-page {
+  background-color: rgba(0, 0, 0, .1);
+}
+
+.main-link {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  padding: 0;
+  color: black;
+  transition: .25s;
+  text-decoration: none;
+  &:hover{
+    gap: 6px;
+  }
+  &__icon {
+    width: 20px;
+    height: 13px;
+  }
+  &__text{
+    font-size: 10px
+  }
 }
 </style>
--- src/pages/Question.vue
+++ src/pages/Question.vue
@@ -7,17 +7,19 @@
     </p>
 
     <div class="question-page__variants">
-      <button @click="selectVariant" v-for="(value, key, index) in question.variants" class="btn btn-primary">{{ value.title }}</button>
+      <button :disabled="answer_text" @click="selectVariant(key, value)" v-for="(value, key, index) in question.variants" class="btn btn-primary">{{ value.title }}</button>
     </div>
 
-
+    <transition name="opacity-transition">
+      <p v-if="answer_text" class="question-page__answer-text">{{answer_text}}</p>
+    </transition>
 <!--    <router-link :to="`/question/${+$route.params.id+1}`" type="button" class="btn btn-primary">{{ button_text }}-->
 <!--    </router-link>-->
 
-    <router-link to="/" class="btn btn-primary">MAIN</router-link>
   </main>
 </template>
 <script setup>
+import {ref} from "vue";
 import {useRoute} from "vue-router";
 import { useQuestionsStore } from "@/stores/questions";
 
@@ -26,13 +28,16 @@ const button_text = "Next";
 
 const $questions_store = useQuestionsStore();
 const question = $questions_store.questions[0];
-
+const answer_text = ref(null);
 
 
 function goToNextQuestion() {
   console.log("go to next question handler");
 }
-function selectVariant(){
-  console.log("select variant handler");
+function selectVariant(key, value){
+  answer_text.value = value.answer;
+
+  if(question.proper != key) return console.error('Wrong');
+  else return console.info('Proper')
 }
 </script>
